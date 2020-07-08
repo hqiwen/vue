e23ecb1af9fde70500f8e107560838019e75c062
stund <1726564970@qq.com>
2020-07-08 10:51:25 +0800

    fix:header 头部高度重合

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,29 +3,29 @@
     <Affix>
       <div class="headerTop primary-bg">
         <el-row class="topContent container">
-            <el-col>
-              <el-row class="social justify-content-start">
-                <a>
-                  <i class="fa fa-facebook"></i>
-                </a>
-                <a>
-                  <i class="fa fa-pinterest"></i>
-                </a>
-                <a>
-                  <i class="fa fa-twitter"></i>
-                </a>
-                <a>
-                  <i class="fa fa-linkedin"></i>
-                </a>
-              </el-row>
-            </el-col>
-            <el-col>
-              <el-row class="languagesAndAccount justify-content-end">
-                <el-button round>ENG</el-button>
-                <el-button round>MY ACCOUNT</el-button>
-              </el-row>
-            </el-col>
-          </el-row>
+          <el-col>
+            <el-row class="social justify-content-start">
+              <a>
+                <i class="fa fa-facebook"></i>
+              </a>
+              <a>
+                <i class="fa fa-pinterest"></i>
+              </a>
+              <a>
+                <i class="fa fa-twitter"></i>
+              </a>
+              <a>
+                <i class="fa fa-linkedin"></i>
+              </a>
+            </el-row>
+          </el-col>
+          <el-col>
+            <el-row class="languagesAndAccount justify-content-end">
+              <el-button round>ENG</el-button>
+              <el-button round>MY ACCOUNT</el-button>
+            </el-row>
+          </el-col>
+        </el-row>
       </div>
 
       <!-- 导航区域 -->
@@ -39,98 +39,122 @@
             <el-menu-item index="1">
               <a>HOME</a>
             </el-menu-item>
-            <el-menu-item index="2"><a>ABOUT US</a></el-menu-item>
+            <el-menu-item index="2">
+              <a>ABOUT US</a>
+            </el-menu-item>
             <el-submenu index="3">
               <template slot="title">SERVICES</template>
-              <el-menu-item index="3-1"><a>SERVICES 1</a></el-menu-item>
-              <el-menu-item index="3-2"><a>SERVICES 2</a></el-menu-item>
-              <el-menu-item index="3-3"><a>SERVICES DETAILS</a></el-menu-item>
+              <el-menu-item index="3-1">
+                <a>SERVICES 1</a>
+              </el-menu-item>
+              <el-menu-item index="3-2">
+                <a>SERVICES 2</a>
+              </el-menu-item>
+              <el-menu-item index="3-3">
+                <a>SERVICES DETAILS</a>
+              </el-menu-item>
             </el-submenu>
             <el-submenu index="4">
               <template slot="title">PAGES</template>
-              <el-menu-item index="4-1"><a>DOCTORS</a></el-menu-item>
-              <el-menu-item index="4-2"><a>DOCTORS DETAILS</a></el-menu-item>
-              <el-menu-item index="4-3"><a>BLOG</a></el-menu-item>
+              <el-menu-item index="4-1">
+                <a>DOCTORS</a>
+              </el-menu-item>
+              <el-menu-item index="4-2">
+                <a>DOCTORS DETAILS</a>
+              </el-menu-item>
+              <el-menu-item index="4-3">
+                <a>BLOG</a>
+              </el-menu-item>
             </el-submenu>
             <el-submenu index="5">
               <template slot="title">BLOG</template>
-              <el-menu-item index="5-1"><a>BLOGS</a></el-menu-item>
-              <el-menu-item index="5-2"><a>BLOGS DETAILS</a></el-menu-item>
+              <el-menu-item index="5-1">
+                <a>BLOGS</a>
+              </el-menu-item>
+              <el-menu-item index="5-2">
+                <a>BLOGS DETAILS</a>
+              </el-menu-item>
             </el-submenu>
             <el-menu-item index="6">
               <a href>CONTACT</a>
             </el-menu-item>
             <el-menu-item index="7">
-              <button class="primary-bg search" @click="handleSearch"><i class="fa fa-search white"></i></button>
+              <button class="primary-bg search" @click="handleSearch">
+                <i class="fa fa-search white"></i>
+              </button>
             </el-menu-item>
           </el-menu>
         </el-col>
         <el-col class="verticalMenu justify-content-end">
-          <button class="" @click="handleOpen"><i class="el-icon-menu"></i></button>
+          <button class @click="handleOpen">
+            <i class="el-icon-menu"></i>
+          </button>
         </el-col>
       </el-row>
       <!-- 导航区域 -->
-      
+
       <!-- 垂直菜单 -->
       <el-row>
-          <el-menu v-if="showMenu" class="navMenu" mode="vertical">
-              <el-menu-item index="1">
-                <a>HOME</a>
-              </el-menu-item>
-              <el-menu-item index="2">ABOUT US</el-menu-item>
-              <el-submenu index="3">
-                <template slot="title">SERVICES</template>
-                <el-menu-item index="3-1">SERVICES 1</el-menu-item>
-                <el-menu-item index="3-2">SERVICES 2</el-menu-item>
-                <el-menu-item index="3-3">SERVICES DETAILS</el-menu-item>
-              </el-submenu>
-              <el-submenu index="4">
-                <template slot="title">PAGES</template>
-                <el-menu-item index="4-1">DOCTORS</el-menu-item>
-                <el-menu-item index="4-2">DOCTORS DETAILS</el-menu-item>
-                <el-menu-item index="4-3">BLOG</el-menu-item>
-              </el-submenu>
-              <el-submenu index="5">
-                <template slot="title">BLOG</template>
-                <el-menu-item index="5-1">ALL BLOGS</el-menu-item>
-                <el-menu-item index="5-2">BLOGS DETAILS</el-menu-item>
-              </el-submenu>
-              <el-menu-item index="6">
-                <a href>CONTACT</a>
-              </el-menu-item>
-              <el-menu-item index="7">
-                <button class="primary-bg search" @click="handleSearch"><i class="fa fa-search white"></i></button>
-              </el-menu-item>
-            </el-menu>
-        </el-row>
+        <el-menu v-if="showMenu" class="navMenu" mode="vertical">
+          <el-menu-item index="1">
+            <a>HOME</a>
+          </el-menu-item>
+          <el-menu-item index="2">ABOUT US</el-menu-item>
+          <el-submenu index="3">
+            <template slot="title">SERVICES</template>
+            <el-menu-item index="3-1">SERVICES 1</el-menu-item>
+            <el-menu-item index="3-2">SERVICES 2</el-menu-item>
+            <el-menu-item index="3-3">SERVICES DETAILS</el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">PAGES</template>
+            <el-menu-item index="4-1">DOCTORS</el-menu-item>
+            <el-menu-item index="4-2">DOCTORS DETAILS</el-menu-item>
+            <el-menu-item index="4-3">BLOG</el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">BLOG</template>
+            <el-menu-item index="5-1">ALL BLOGS</el-menu-item>
+            <el-menu-item index="5-2">BLOGS DETAILS</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="6">
+            <a href>CONTACT</a>
+          </el-menu-item>
+          <el-menu-item index="7">
+            <button class="primary-bg search" @click="handleSearch">
+              <i class="fa fa-search white"></i>
+            </button>
+          </el-menu-item>
+        </el-menu>
+      </el-row>
     </Affix>
   </header>
 </template>
 
 <script>
-import Affix from './Affix'
+import Affix from "./Affix";
 
 export default {
   data() {
     return {
       showSearch: false,
       showMenu: false
-    }
+    };
   },
   methods: {
     handleOpen() {
-      this.showMenu = !this.showMenu
+      this.showMenu = !this.showMenu;
     },
     handleSearch() {
       this.$prompt(null, null, {
-          confirmButtonText: '搜索',
-          cancelButtonText: '取消',
-        }).then(({ value }) => {
-          this.$message({
-            type: 'success',
-            message: '你的搜索内容: ' + value
-          });
-        })
+        confirmButtonText: "搜索",
+        cancelButtonText: "取消"
+      }).then(({ value }) => {
+        this.$message({
+          type: "success",
+          message: "你的搜索内容: " + value
+        });
+      });
     }
   },
   components: {
@@ -147,14 +171,13 @@ export default {
 
 @media screen and (min-width: 575px) {
   .verticalMenu.justify-content-end {
-    display: none
+    display: none;
   }
 }
 
 @media (max-width: 575px) {
-
   .horizontalMenu.justify-content-end {
-    display: none
+    display: none;
   }
 
   .topContent {
@@ -172,7 +195,7 @@ export default {
 }
 
 .verticalMenu .el-icon-menu {
-  font-size: 3rem
+  font-size: 3rem;
 }
 
 .headerTop {
@@ -221,8 +244,8 @@ export default {
   background: none;
 }
 
-.navMenu.el-menu--horizontal>.el-menu-item.is-active {
-  border: none
+.navMenu.el-menu--horizontal > .el-menu-item.is-active {
+  border: none;
 }
 
 .navMenu .search {
@@ -244,4 +267,7 @@ export default {
   outline: none;
 }
 
+.nav img {
+  height: 77px;
+}
 </style>
