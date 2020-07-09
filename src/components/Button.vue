652a17a5b2b990fb89e46c911d1dc1bcd92dedb8
stund <1726564970@qq.com>
2020-07-09 11:36:09 +0800

    refactor button compontent

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,52 +1,47 @@
 <template>
-    <a v-if="color === 'white'" class="white-btn-fill" >
-        <slot></slot>
-    </a>
-    <a v-else class="btn-fill">
-        <slot></slot>
-    </a>
+  <a class="btn-fill" :class="white ? 'white' : null">
+    <slot></slot>
+  </a>
 </template>
 
 <script>
 export default {
-    props: {
-        color: {
-            type: String,
-            default: ''
-        }
+  props: {
+    white: {
+      type: Boolean
     }
+  }
 };
 </script>
 <style scoped>
-
 a {
-    font-size: 16px;
-    font-weight: 600;
-    padding: 14px 45px;
-    display: inline-block;
-    border-radius: 50px;
-    cursor: pointer;
-    text-transform: uppercase;
-    transition: 0.4s;
+  font-size: 16px;
+  font-weight: 600;
+  padding: 14px 45px;
+  display: inline-block;
+  border-radius: 50px;
+  cursor: pointer;
+  text-transform: uppercase;
+  transition: 0.4s;
 }
 
 a.btn-fill {
-    background: #00C9B7;
-    color: #fff;
+  background: #00c9b7;
+  color: #fff;
 }
 
-a.btn-fill:hover{
-    background: #fff;
-    color: #00C9B7;
+a.btn-fill:hover {
+  background: #fff;
+  color: #00c9b7;
 }
 
-a.white-btn-fill {
-    background: #fff;
-    color:#00C9B7
+a.btn-fill.white {
+  background: #fff;
+  color: #00c9b7;
 }
 
-a.white-btn-fill:hover {
-    background: #00C9B7;
-    color: #fff;
+a.btn-fill.white:hover {
+  background: #00c9b7;
+  color: #fff;
 }
 </style>
